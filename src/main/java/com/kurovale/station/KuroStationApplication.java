1e28b009cce0c9f964526eb3a64e98d010612add
kuro-vale <jsalcedo218@gmail.com>
2022-05-10 15:27:06 -0500

    Initial commit

---
+++ src/main/java/com/kurovale/station/KuroStationApplication.java
@@ -0,0 +1,13 @@
+package com.kurovale.station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuroStationApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(KuroStationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kurovale/station/KuroStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurovale.station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuroStationApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
+}
